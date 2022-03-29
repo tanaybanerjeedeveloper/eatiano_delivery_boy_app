2e0170684fc9c250d117a5e413b91b27b540930c
tanaybanerjeedeveloper <banerjeetanay333@gmail.com>
2022-03-29 16:39:26 +0530

    file and img uploading done

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -7,6 +7,11 @@ import 'dart:io';
 import '../constants.dart';
 import 'package:open_file/open_file.dart';
 import 'package:file_picker/file_picker.dart';
+import '../widgets/button.dart';
+import '../services/networking_service.dart';
+import 'package:dio/dio.dart';
+import 'package:path/path.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileScreen extends StatefulWidget {
   static const id = 'Register';
@@ -17,8 +22,55 @@ class ProfileScreenState extends State<ProfileScreen> {
   final _passwordFocus = FocusNode();
   File? image;
   String? name;
+  String? phone;
+  String? country;
+  String? aadharNo;
+  String? address;
   FilePickerResult? result;
   PlatformFile? file;
+
+  @override
+  void initState() {
+    super.initState();
+    // var result = _getUserDetails();
+    // print('result: $result');
+    _getUserDetails();
+  }
+
+  Future<void> _getUserDetails() async {
+    NetworkingService networkingService = NetworkingService(
+        'https://achievexsolutions.in/current_work/eatiano/api/auth/me');
+    var result = await networkingService.getData();
+    print('result: $result');
+    setState(() {
+      name = result['name'];
+      phone = result['phone'];
+      country = result['country'];
+    });
+  }
+
+  _submit(filePath) async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    String fileName = basename(filePath.path);
+    var formData = FormData.fromMap({
+      "photo": await MultipartFile.fromFile(image!.path),
+      "idprof": await MultipartFile.fromFile(filePath.path, filename: fileName),
+      "name": name,
+      "phone": phone,
+      "country": country,
+      "address": address,
+      "adhar": aadharNo
+    });
+
+    var response = await Dio().post(
+        'https://achievexsolutions.in/current_work/eatiano/api/delivery/edit_delivery',
+        data: formData,
+        options: Options(headers: {
+          'Authorization': 'Bearer ${localStorage.getString('token')}',
+          'Accept': 'application/json'
+        }));
+    print('upload response: $response');
+  }
 
   Future pickImage() async {
     print('image selected');
@@ -34,6 +86,17 @@ class ProfileScreenState extends State<ProfileScreen> {
     }
   }
 
+  Future<void> pickFiles() async {
+    result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+    file = result!.files.first;
+    viewFile(file!);
+  }
+
+  void viewFile(PlatformFile file) {
+    OpenFile.open(file.path);
+  }
+
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
@@ -43,13 +106,6 @@ class ProfileScreenState extends State<ProfileScreen> {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back_ios,
-            color: Theme.of(context).primaryColor,
-          ),
-          onPressed: () => Navigator.pop(context),
-        ),
         backgroundColor: const Color.fromRGBO(25, 29, 33, 1),
         //backgroundColor: Colors.amber,
         elevation: 0,
@@ -123,21 +179,13 @@ class ProfileScreenState extends State<ProfileScreen> {
                     ]),
                 child: TextFormField(
                   decoration: InputDecoration(
-                    label: Text('Name'),
+                    // label: Text('Name'),
+                    hintText: name,
                     focusedBorder: InputBorder.none,
                   ),
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) =>
-                      FocusScope.of(context).requestFocus(_passwordFocus),
-                  validator: (nameInput) {
-                    if (nameInput!.isEmpty) {
-                      return 'Please Enter Name';
-                    } else {
-                      name = nameInput;
-                      return null;
-                    }
-                  },
+                  onChanged: (name) => name = name,
                 ),
               ),
               SizedBox(height: height * 0.03),
@@ -156,21 +204,11 @@ class ProfileScreenState extends State<ProfileScreen> {
                     ]),
                 child: TextFormField(
                   decoration: InputDecoration(
-                    label: Text('Phone'),
+                    hintText: phone,
                     focusedBorder: InputBorder.none,
                   ),
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) =>
-                      FocusScope.of(context).requestFocus(_passwordFocus),
-                  validator: (nameInput) {
-                    if (nameInput!.isEmpty) {
-                      return 'Please Enter Name';
-                    } else {
-                      name = nameInput;
-                      return null;
-                    }
-                  },
                 ),
               ),
               SizedBox(height: height * 0.03),
@@ -189,21 +227,11 @@ class ProfileScreenState extends State<ProfileScreen> {
                     ]),
                 child: TextFormField(
                   decoration: InputDecoration(
-                    label: Text('Country'),
+                    hintText: country,
                     focusedBorder: InputBorder.none,
                   ),
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) =>
-                      FocusScope.of(context).requestFocus(_passwordFocus),
-                  validator: (nameInput) {
-                    if (nameInput!.isEmpty) {
-                      return 'Please Enter Name';
-                    } else {
-                      name = nameInput;
-                      return null;
-                    }
-                  },
                 ),
               ),
               SizedBox(height: height * 0.03),
@@ -227,16 +255,35 @@ class ProfileScreenState extends State<ProfileScreen> {
                   ),
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) =>
-                      FocusScope.of(context).requestFocus(_passwordFocus),
-                  validator: (nameInput) {
-                    if (nameInput!.isEmpty) {
-                      return 'Please Enter Name';
-                    } else {
-                      name = nameInput;
-                      return null;
-                    }
-                  },
+                  onChanged: (value) => setState(() {
+                    aadharNo = value;
+                  }),
+                ),
+              ),
+              SizedBox(height: height * 0.03),
+              Container(
+                height: height * 0.06,
+                padding: EdgeInsets.only(left: width * 0.045),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 20,
+                          spreadRadius: 10,
+                          offset: Offset(2, 1))
+                    ]),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    label: Text('Address'),
+                    focusedBorder: InputBorder.none,
+                  ),
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.next,
+                  onChanged: (value) => setState(() {
+                    address = value;
+                  }),
                 ),
               ),
               SizedBox(height: height * 0.03),
@@ -252,22 +299,18 @@ class ProfileScreenState extends State<ProfileScreen> {
                       ),
                       Text('Pick File'),
                     ],
-                  ))
+                  )),
+              SizedBox(height: height * 0.03),
+              InkWell(
+                onTap: () {
+                  _submit(file);
+                },
+                child: Button('Submit'),
+              )
             ],
           ),
         ),
       ),
     );
-  }
-
-  Future<void> pickFiles() async {
-    result = await FilePicker.platform.pickFiles();
-    if (result == null) return;
-    file = result!.files.first;
-    viewFile(file!);
-  }
-
-  void viewFile(PlatformFile file) {
-    OpenFile.open(file.path);
   }
 }
---
+++ lib/services/networking_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:http/http.dart' as http;
+
+class NetworkingService {
+  String url;
+
+  NetworkingService(this.url);
+  Future<dynamic> getData() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var response = await http.get(Uri.parse(url), headers: {
+      'Authorization': 'Bearer ${localStorage.getString('token')}',
+      'Accept': 'application/json'
+    });
+    if (response.statusCode == 200) {
+      var data = response.body;
+      print('data: ${jsonDecode(data)}');
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
